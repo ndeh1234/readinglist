e3a93697615fcb510d92793e6c32c8249b31e9c8
ndeh1234 <30512965+ndeh1234@users.noreply.github.com>
2019-02-02 23:10:56 -0600

    Handle book_add
    I am handling book_add if book is already in the store

--- main.py
+++ main.py
@@ -35,10 +35,19 @@ def create_menu():
 
 
 def add_book():
+    
     new_book = ui.get_book_info()
+    
     print("\n") # adding blank line after list of books
+    all_books = store.get_all_books()
+    if new_book in all_books:
+        ui.message('The book already exists')
+    else:
     store.add_book(new_book)
+        
     print("\n") # adding blank line after list of books
+    ui.message('New book Added!')
+   
     # TODO show an error message if a book is already in the store, don't add book
 
 
