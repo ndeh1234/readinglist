eb869b2286f53da45cb1ccc46b1d1a05ed17448f
ndeh1234 <30512965+ndeh1234@users.noreply.github.com>
2019-02-02 23:02:48 -0600

    Adding Try and except blocks
     I am adding try and except blocks

--- main.py
+++ main.py
@@ -74,8 +74,15 @@ def search_book():
 def change_read():
 
     book_id = ui.get_book_id()
-    new_read = ui.get_read_value()
-    store.set_book_read(book_id, new_read)
+    try:                               #Try statement will search the store to get the book name by its book_id
+        book = store.get_book(book_id)
+    except:                            # If book is not found or the book_id is incorrect etc, then the 'except' block will be executed
+        ui.message('The book is not in Store!')
+    else:                              #else as the book name will be retrieved from the store
+                                       #it will be certain that the book exists, hence the book_read_value() will be retrieved.
+        new_read = ui.get_read_value()
+        store.set_book_read(book_id, new_read)
+   
     # TODO show error message if book's ID is not found.
 
 
